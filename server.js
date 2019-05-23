c69eea17a1bfcb87b4dfab324d71d591886140ef
Aleksandar Ristic <storiaca@gmail.com>
2019-05-23 14:56:29 +0200

    Request and body validation

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const auth = require("./routes/api/auth");
 const posts = require("./routes/api/posts");
 // Connect Database
 connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("API Running"));
 
 // Define Routes
@@ -16,6 +20,7 @@ app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/auth", auth);
 app.use("/api/posts", posts);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
