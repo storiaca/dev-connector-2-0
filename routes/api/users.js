ec23bdbc78692fe104bbb1da43288d988969606b
storiaca <storiaca@gmail.com>
2020-03-24 10:04:21 +0100

    User registration finish

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator/check");
 
 const User = require("../../models/User");
@@ -31,10 +33,37 @@ router.post(
     try {
       // See if user exist
       let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ erors: [{ msg: "User already exists" }] });
+      }
+
       // Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: "200", // Size of image
+        r: "pg", // Rating of image
+        d: "mm" // Default image
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
       // Encrypt password
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
       // Return jsonwebtoken
-      res.send("User route");
+
+      res.send("User registered");
     } catch (err) {
       console.log(err.message);
       res.status(500).send("Server error");
