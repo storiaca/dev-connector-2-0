720aac1b9efcf5f0eda17da58984b751f27bb2b6
storiaca <storiaca@gmail.com>
2020-03-27 12:19:26 +0100

    Get all profiles and profile by user name

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -28,7 +28,7 @@ router.get("/me", auth, async (req, res) => {
 });
 
 /**
- * @route   GET api/profile
+ * @route   POST api/profile
  * @desc    Create or update user profile
  * @access  Private
  */
@@ -114,4 +114,41 @@ router.post(
   }
 );
 
+/**
+ * @route   GET api/profile
+ * @desc    Get all profiles
+ * @access  Public
+ */
+
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("user", ["name", "avatar"]);
+    res.json(profiles);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+/**
+ * @route   GET api/profile/user/:user_id
+ * @desc    Get profile by user id
+ * @access  Public
+ */
+
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }).populate("user", ["name", "avatar"]);
+    if (!profile) return res.status(400).json({ msg: "Profile not found" });
+    res.json(profile);
+  } catch (err) {
+    console.log(err.message);
+    if (err.kind == "Object Id") {
+      return res.status(400).json({ msg: "Profile not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
 module.exports = router;
