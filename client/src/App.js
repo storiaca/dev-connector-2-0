864c62ceed4672a095a223250beb66e960443da2
Software/Nation <aristic@software-nation.com>
2020-08-03 17:05:07 +0200

    Router start

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 import Landing from "./components/layout/Landing";
 import "./App.css";
 const App = () => {
   return (
-    <>
-      <Navbar />
-      <Landing />
-    </>
+    <Router>
+      <>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+      </>
+    </Router>
   );
 };
 
