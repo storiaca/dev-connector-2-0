f770b4948999c86ac5b7df996797ed87cdbe7f74
Aleksandar Ristic <storiaca@gmail.com>
2020-08-15 13:14:01 +0200

    Request example start

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -13,12 +14,24 @@ const Register = () => {
 
   const { name, email, password, password2 } = formData;
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2) {
       console.log("Passwords do not match");
     } else {
-      console.log(formData);
+      const newUser = {
+        name,
+        email,
+        password,
+      };
+      try {
+        const config = {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        };
+        const body = JSON.stringify(newUser);
+      } catch (err) {}
     }
   };
   return (
