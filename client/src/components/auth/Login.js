c794f97cdbb06790a7faa5d693029fa045582e54
Aleksandar Ristic <storiaca@gmail.com>
2020-08-20 17:00:58 +0200

    Create Login component

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = (props) => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const onChangeLoginForm = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const { email, password } = formData;
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log("Success");
+  };
   return (
-    <div>
-      <h2>Login</h2>
-    </div>
+    <section className="container">
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Sign Into Your Account
+      </p>
+      <form className="form" onSubmit={(e) => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            value={email}
+            onChange={(e) => onChangeLoginForm(e)}
+            name="email"
+            required
+          />
+          <small className="form-text">
+            This site uses Gravatar so if you want a profile image, use a
+            Gravatar email
+          </small>
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={(e) => onChangeLoginForm(e)}
+            minLength="6"
+          />
+        </div>
+
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1">
+        Already have an account? <a href="login.html">Sign In</a>
+      </p>
+    </section>
   );
 };
 
