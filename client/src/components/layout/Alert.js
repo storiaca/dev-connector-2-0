315b76da29fa9a7c2130e77abddbdfa5a4e7287c
Aleksandar Ristic <storiaca@gmail.com>
2020-09-04 17:38:31 +0200

    Alert component import in App

--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -3,14 +3,18 @@ import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
 const Alert = ({ alerts }) =>
-  alerts !== null && alerts.length > 0 && alerts.map(alert => (
-    
+  alerts !== null &&
+  alerts.length > 0 &&
+  alerts.map((alert) => (
+    <div key={alert.id} className={`alert alert-${alert.alertType}`}>
+      {alert.msg}
+    </div>
   ));
 
 Alert.propTypes = {};
 
-const mapStateToProps = (state = {
+const mapStateToProps = (state) => ({
   alerts: state.alert,
 });
 
-export default connect()(Alert);
+export default connect(mapStateToProps)(Alert);
