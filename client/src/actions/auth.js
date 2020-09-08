9bd87533219dd92a9511571aac591fbd5f2c1c2e
Aleksandar Ristic <storiaca@gmail.com>
2020-09-08 17:12:25 +0200

    Add new types

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 import { setAlert } from "./alert";
 
-import { REGISTER_SUCCESS, REGISTER_FAIL } from "./types";
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+} from "./types";
 
 // Register User
 export const register = ({ name, email, password }) => async (dispatch) => {
