838b2e28650c56c9f19de902c6acfc44985fc636
Aleksandar Ristic <storiaca@gmail.com>
2020-09-15 17:33:15 +0200

    Add loadUser to App.ja

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,18 @@ import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Landing from "./components/layout/Landing";
 import Alert from "./components/layout/Alert";
+import setAuthToken from "./utils/setAuthToken";
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
+import { loadUser } from "./actions/auth";
 
 import "./App.css";
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
 const App = () => {
   return (
     <Provider store={store}>
