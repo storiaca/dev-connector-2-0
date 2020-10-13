e08520748a8ca5b019d8eaa4e2681b4b4ea24f59
Aleksandar Ristic <storiaca@gmail.com>
2020-10-13 15:04:56 +0200

    Starting on the Dashboard

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -10,6 +10,7 @@ import {
   LOGIN_SUCCESS,
   LOGIN_FAIL,
   LOGOUT,
+  CLEAR_PROFILE,
 } from "./types";
 
 // Load User
@@ -94,5 +95,6 @@ export const login = (email, password) => async (dispatch) => {
 
 // Logout / Clear Profile
 export const logout = () => (dispatch) => {
+  dispatch({ type: CLEAR_PROFILE });
   dispatch({ type: LOGOUT });
 };
