1f7fe76b377c9bce27b9145b2592828094d5e3fa
Software/Nation <storiaca@gmail.com>
2020-10-16 17:00:00 +0200

    Create createprofile component

---
+++ client/src/components/profile-forms/CreateProfile.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const CreateProfile = props => {
+  return (
+    <div>
+      <h2>Create Profile</h2>
+    </div>
+  )
+}
+
+CreateProfile.propTypes = {
+
+}
+
+export default CreateProfile
